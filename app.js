216742fea8fb292cd22f88b7c58dd6214135636e
d4vwdi <d4vwdi@gmail.com>
2023-02-15 10:20:57 -0300

    teste servidor node.js

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
-var soma = require('./soma.js')
-var subtracao = require('./subtracao.js')
-var multiplicacao = require('./multiplicacao.js')
-var divisao = require('./divisao.js')
 
-console.log(soma(10,10))
-console.log(subtracao(10,10))
-console.log(multiplicacao(10,10))
-console.log(divisao(10,10))
+var http = require(http)
+http.createServer(function(req, res){
+    res.end("Mensagem direcionada!")
+}).listen(8081)
 
+console.log(Servidor Ativo!)
+
+
+//CARREGAMENTO DE MÓDULOS
+//var soma = require('./soma.js')
+//var subtracao = require('./subtracao.js')
+//var multiplicacao = require('./multiplicacao.js')
+//var divisao = require('./divisao.js')
+//
+//console.log(soma(10,10))
+//console.log(subtracao(10,10))
+//console.log(multiplicacao(10,10))
+//console.log(divisao(10,10))
+//FIM DO CARREGAMENTO DE MÓDULOS
+
+//TESTE LÓGICO SE (IF)
 // var a = 10
 
 //     if(a <= 10){
@@ -15,8 +27,10 @@ console.log(divisao(10,10))
 //     }else{
 //         console.log("A variável a é maior que 10")
 //     }
+//FIM DO TESTE LÓGICO SE (IF)
 
-//    ETAPA 1 
+
+//REALIZANDO OPERAÇÕES DE CALCULADORA
 // var b = 30
 // var total = 0
 
@@ -35,6 +49,8 @@ console.log(divisao(10,10))
 // //Divisão
 // total = a / b
 // console.log(total)
+//FIM DAS OPERAÇÕES DE CALCULADORA
 
+//MENSAGEM NO TERMINAL DE COMANDO
 // console.log ("Primeira aula de Node JS")
 
