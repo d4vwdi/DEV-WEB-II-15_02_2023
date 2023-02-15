fc27ea41503208e1a3d5e61480fa16e7f6a970f9
d4vwdi <d4vwdi@gmail.com>
2023-02-15 09:11:54 -0300

    criação app node.js

---
+++ app.js
@@ -0,0 +1,30 @@
+var a = 10
+
+    if(a <= 10){
+        console.log("A variável a é menor ou igual a 10")
+    }else{
+        console.log("A variável a é maior que 10")
+    }
+
+    
+// var b = 30
+// var total = 0
+
+// //Adição
+// total = a + b
+// console.log(total)
+
+// //Subtração
+// total = a - b
+// console.log(total)
+
+// //Multiplicação
+// total = a * b
+// console.log(total)
+
+// //Divisão
+// total = a / b
+// console.log(total)
+
+// console.log ("Primeira aula de Node JS")
+
