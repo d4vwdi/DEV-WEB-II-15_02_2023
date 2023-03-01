c4ec2838646107b6e70f01bc7ac1787d5bc2bd1d
d4vwdi <d4vwdi@gmail.com>
2023-03-01 09:20:54 -0300

    rota para o diretório principal do domínio

--- DEV-WEB-II-01_03_2023/app.js
+++ DEV-WEB-II-01_03_2023/app.js
@@ -1,6 +1,13 @@
 const express = require("express")
 const app = express()
 
+//Criando uma rota para o diretório principal do domínio
+app.get("/", function(req, res){
+    res.send("Estou na primeira página.")
+})
+
+//Criando o servidor web na porta 8081
 app.listen(8081, function(){
     console.log("Servidor Ativo!")
 })
+//Nada pode vir após o listen. pois ele encerra a leitura do browser
