dc2bc71e0710114d82bab1374eff1896027acf1b
d4vwdi <d4vwdi@gmail.com>
2023-03-01 09:43:02 -0300

    adicionando páginas para teste

--- DEV-WEB-II-01_03_2023/app.js
+++ DEV-WEB-II-01_03_2023/app.js
@@ -3,11 +3,15 @@ const app = express()
 
 //Criando uma rota para o diretório principal do domínio
 app.get("/", function(req, res){
-    res.send("Estou na primeira página.")
+    res.send("<h1>Estou na primeira página.</h1>")
 })
 //Teste com uma outra página
 app.get("/contato", function(req, res){
     res.send("Estou na página de contato.")
+})
+//Teste mais uma outra página
+app.get("/produto", function(req, res){
+    res.send("Estou na página de produto.")
 })
 //Criando o servidor web na porta 8081
 app.listen(8081, function(){
