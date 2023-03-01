9bcb87c0e2e2e32a6ce52bf7f5a56de3e73781b6
d4vwdi <d4vwdi@gmail.com>
2023-03-01 11:17:39 -0300

    adicionando pagina html

--- DEV-WEB-II-01_03_2023/app.js
+++ DEV-WEB-II-01_03_2023/app.js
@@ -3,7 +3,8 @@ const app = express()
 
 //Criando uma rota para o diretório principal do domínio
 app.get("/", function(req, res){
-    res.send("<h1>Estou na primeira página.</h1>")
+    //res.send("<h1>Estou na primeira página.</h1>")
+    res.sendFile(__dirname + "/html/index.html")
 })
 //Teste com uma outra página
 app.get("/contato", function(req, res){
@@ -26,3 +27,4 @@ app.listen(8081, function(){
     console.log("Servidor Ativo!")
 })
 //Nada pode vir após o listen. pois ele encerra a leitura do browser
+
